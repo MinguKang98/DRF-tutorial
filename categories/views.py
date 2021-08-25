467768d5c37ed483dc16937eac9784b696fa0477
MinguKang98 <alsrn7590@naver.com>
2021-08-25 23:09:02 +0900

    Refactor categories using generic CBV
    Refactor views of categoreis using generic CBV

--- categories/views.py
+++ categories/views.py
@@ -1,37 +1,15 @@
-from rest_framework import mixins
 from rest_framework import generics
 from . import models
 from . import serializers
 
 
-class CategoryList(
-    generics.GenericAPIView, mixins.CreateModelMixin, mixins.ListModelMixin
-):
-    queryset = models.Category.objects.all()
-    serializer_class = serializers.CategorySerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class CategoryDetail(
-    generics.GenericAPIView,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-):
+class CategoryList(generics.ListCreateAPIView):
 
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
